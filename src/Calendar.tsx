b57feb6e29df20940096a0993147b4cd9607fe0f
Shiba <38932286+KaitakuShiba@users.noreply.github.com>
2021-09-02 16:38:07 +0900

    add-cal-content

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,9 +1,19 @@
 import { Square } from "./Square";
+import styled from "@emotion/styled";
 
 export const Calendar = () => {
+  const Wrapper = styled("div")`
+    margin: 20px;
+    border-left: 1px solid #898989;
+    border-top: 1px solid #898989;
+    width: 420px;
+  `;
+
   return (
-    <div>
-      <Square />
-    </div>
+    <>
+      <Wrapper>
+        <Square />
+      </Wrapper>
+    </>
   );
 };
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,14 +1,39 @@
-import React from "react";
 import styled from "@emotion/styled";
 
 export const Square = () => {
-  const Hello = styled("div")`
-    color: red;
+  const Box = styled("div")`
+    border-right: 1px solid #898989;
+    border-bottom: 1px solid #898989;
+    width: 60px;
+    height: 60px;
   `;
 
+  const Row = styled("div")`
+    display: flex;
+    flex-direction: row;
+  `;
+
+  const Date = styled("div")`
+    top: 40px;
+    left: 45px;
+    position: relative;
+  `;
+
+  const renderRowSquares = () => {
+    return [1, 2, 3, 4, 5, 6, 7].map((i) => (
+      <Box>
+        <Date>{i}</Date>
+      </Box>
+    ));
+  };
+
   return (
-    <React.Fragment>
-      <Hello>Square</Hello>
-    </React.Fragment>
+    <>
+      <Row>{renderRowSquares()}</Row>
+      <Row>{renderRowSquares()}</Row>
+      <Row>{renderRowSquares()}</Row>
+      <Row>{renderRowSquares()}</Row>
+      <Row>{renderRowSquares()}</Row>
+    </>
   );
 };
