a7f10dc1278caaebfb85a349214a9c73aa0466ad
Shiba <38932286+KaitakuShiba@users.noreply.github.com>
2021-09-02 16:53:17 +0900

    add-days

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,9 +1,10 @@
 import { Square } from "./Square";
+import { Days } from "./Days";
 import styled from "@emotion/styled";
 
 export const Calendar = () => {
   const Wrapper = styled("div")`
-    margin: 20px;
+    margin: 0 20px 20px 20px;
     border-left: 1px solid #898989;
     border-top: 1px solid #898989;
     width: 420px;
@@ -11,6 +12,7 @@ export const Calendar = () => {
 
   return (
     <>
+      <Days />
       <Wrapper>
         <Square />
       </Wrapper>
---
+++ src/Days.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+
+export const Days = () => {
+  const Wrapper = styled("div")`
+    width: 60px;
+    height: 60px;
+  `;
+
+  const Row = styled("div")`
+    display: flex;
+    flex-direction: row;
+  `;
+
+  const Day = styled("div")`
+    top: 40px;
+    left: 45px;
+    position: relative;
+    font-family: Roboto;
+    font-size: 14px;
+    font-style: normal;
+    letter-spacing: 0em;
+    text-align: left;
+  `;
+
+  const renderDays = () => {
+    return ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"].map((i) => (
+      <Wrapper>
+        <Day>{i}</Day>
+      </Wrapper>
+    ));
+  };
+
+  return (
+    <>
+      <Row>{renderDays()}</Row>
+    </>
+  );
+};
